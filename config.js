c3e7a9e4734bd7826dba08ad2de58ee48f7efdbf
lisalisa122051-droid <lisalisa122051@gmail.com>
2025-12-30 18:50:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  botName: 'Liviaa Bot MD',
-  ownerName: 'Andiji',
-  ownerNumber: ['628xxxxxxxxx'],
-  prefix: '.',
-  mode: 'public'
+  botName: "Liviaa Bot MD",
+  ownerName: "Andiji",
+  ownerNumber: ["628xxxxxxxxxx"], // ganti
+  prefix: ".",
+  mode: "public", // public / self
+  timezone: "Asia/Jakarta"
 }
